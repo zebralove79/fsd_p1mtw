546a6fe7b9545b650917f16819e26600679d5525
zebralove79 <zebralove79@gmail.com>
2015-10-14 20:28:15 +0200

    Adjusted length of code lines to be <80 chars.

--- favourite_movies.py
+++ favourite_movies.py
@@ -9,50 +9,56 @@ logging.basicConfig(level=logging.ERROR,
 logging.debug('Filling movie \'database\'')
 
 # Define / create Movie instances for six favourite-ish films
-electric_dragon = movie.Movie('Electric Dragon 80.000 V', 2001,
-                              r'https://upload.wikimedia.org/wikipedia/en/9/91/Electricdragon.jpg',
-                              r'https://www.youtube.com/watch?v=0nHfldl5FzQ',
-                              'Gakuryû Ishii', 55,
-                              (('IMDB', r'http://www.imdb.com/title/tt0276935/'),
-                               ('Wikipedia', r'https://en.wikipedia.org/wiki/Electric_Dragon_80.000_V')))
+electric_dragon = movie.Movie(
+    'Electric Dragon 80.000 V', 2001,
+    r'https://upload.wikimedia.org/wikipedia/en/9/91/Electricdragon.jpg',
+    r'https://www.youtube.com/watch?v=0nHfldl5FzQ',
+    'Gakuryû Ishii', 55,
+    (('IMDB', r'http://www.imdb.com/title/tt0276935/'),
+     ('Wikipedia', r'https://en.wikipedia.org/wiki/Electric_Dragon_80.000_V')))
 
-midnight_in_paris = movie.Movie('Midnight in Paris', 2011,
-                                r'https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg',
-                                r'https://www.youtube.com/watch?v=FAfR8omt-CY',
-                                'Woody Allen', 94,
-                                (('IMDB', r'http://www.imdb.com/title/tt1605783/'),
-                                 ('Wikipedia', r'https://en.wikipedia.org/wiki/Midnight_in_Paris'),
-                                 ('Website', r'http://www.sonyclassics.com/midnightinparis/')))
+midnight_in_paris = movie.Movie(
+    'Midnight in Paris', 2011,
+    r'http://bit.ly/1LMxGFR',
+    r'https://www.youtube.com/watch?v=FAfR8omt-CY',
+    'Woody Allen', 94,
+    (('IMDB', r'http://www.imdb.com/title/tt1605783/'),
+     ('Wikipedia', r'https://en.wikipedia.org/wiki/Midnight_in_Paris'),
+     ('Website', r'http://www.sonyclassics.com/midnightinparis/')))
 
-manos = movie.Movie('Manos: The Hands of Fate', 1966,
-                    r'https://upload.wikimedia.org/wikipedia/commons/f/fe/Manosposter.jpg',
-                    r'https://www.youtube.com/watch?v=tRcGukCdr3c',
-                    'Harold P. Warren', 75,
-                    (('IMDB', r'http://www.imdb.com/title/tt0060666/'),
-                     ('Wikipedia', r'https://en.wikipedia.org/wiki/Manos:_The_Hands_of_Fate')))
+manos = movie.Movie(
+    'Manos: The Hands of Fate', 1966,
+    r'https://upload.wikimedia.org/wikipedia/commons/f/fe/Manosposter.jpg',
+    r'https://www.youtube.com/watch?v=tRcGukCdr3c',
+    'Harold P. Warren', 75,
+    (('IMDB', r'http://www.imdb.com/title/tt0060666/'),
+     ('Wikipedia', r'https://en.wikipedia.org/wiki/Manos:_The_Hands_of_Fate')))
 
-inception = movie.Movie('Inception', 2010,
-                        r'https://upload.wikimedia.org/wikipedia/en/7/7f/Inception_ver3.jpg',
-                        r'https://www.youtube.com/watch?v=d3A3-zSOBT4',
-                        'Christopher Nolan', 148,
-                        (('IMDB', r'http://www.imdb.com/title/tt1375666/'),
-                         ('Wikipedia', r'https://en.wikipedia.org/wiki/Inception'),
-                         ('Website', r'http://www.warnerbros.com/inception')))
+inception = movie.Movie(
+    'Inception', 2010,
+    r'https://upload.wikimedia.org/wikipedia/en/7/7f/Inception_ver3.jpg',
+    r'https://www.youtube.com/watch?v=d3A3-zSOBT4',
+    'Christopher Nolan', 148,
+    (('IMDB', r'http://www.imdb.com/title/tt1375666/'),
+     ('Wikipedia', r'https://en.wikipedia.org/wiki/Inception'),
+     ('Website', r'http://www.warnerbros.com/inception')))
 
-wall_e = movie.Movie('WALL-E', 2008,
-                     r'https://upload.wikimedia.org/wikipedia/en/c/c2/WALL-Eposter.jpg',
-                     r'https://www.youtube.com/watch?v=alIq_wG9FNk',
-                     'Andrew Stanton', 98,
-                     (('IMDB', r'http://www.imdb.com/title/tt0910970/'),
-                      ('Wikipedia', r'https://en.wikipedia.org/wiki/WALL-E'),
-                      ('Website', r'http://movies.disney.com/wall-e')))
+wall_e = movie.Movie(
+    'WALL-E', 2008,
+    r'https://upload.wikimedia.org/wikipedia/en/c/c2/WALL-Eposter.jpg',
+    r'https://www.youtube.com/watch?v=alIq_wG9FNk',
+    'Andrew Stanton', 98,
+    (('IMDB', r'http://www.imdb.com/title/tt0910970/'),
+     ('Wikipedia', r'https://en.wikipedia.org/wiki/WALL-E'),
+     ('Website', r'http://movies.disney.com/wall-e')))
 
-pans_labyrinth = movie.Movie('Pan\' Labyrinth', 2006,
-                             r'https://upload.wikimedia.org/wikipedia/en/6/67/Pan%27s_Labyrinth.jpg',
-                             r'https://www.youtube.com/watch?v=EqYiSlkvRuw',
-                             'Guillermo del Toro', 118,
-                             (('IMDB', r'http://www.imdb.com/title/tt0457430/'),
-                              ('Wikipedia', r'https://en.wikipedia.org/wiki/Pan%27s_Labyrinth')))
+pans_labyrinth = movie.Movie(
+    'Pan\' Labyrinth', 2006,
+    r'https://upload.wikimedia.org/wikipedia/en/6/67/Pan%27s_Labyrinth.jpg',
+    r'https://www.youtube.com/watch?v=EqYiSlkvRuw',
+    'Guillermo del Toro', 118,
+    (('IMDB', r'http://www.imdb.com/title/tt0457430/'),
+     ('Wikipedia', r'https://en.wikipedia.org/wiki/Pan%27s_Labyrinth')))
 
 # Create list of films and populate it with the Movie instances
 movies = list()
